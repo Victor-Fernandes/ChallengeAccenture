f21c0e66593872ec2bf869895a63a42d2683bec1
alexsilva <alex.silva@informata.com.br>
2020-10-03 16:48:08 -0300

    DEV-alex | comit 3

--- backend/accenturemobilenokia/src/App.js
+++ backend/accenturemobilenokia/src/App.js
@@ -5,18 +5,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      
+      <span>Accenture</span>
+      
       <header className="App-header">
-        <img src={logo}  alt="moblie" usemap="#image-map"/>
-
+        <img src={logo}  alt="moblie" />
+      
       </header>
-        
-        <div name="image-map">
-          <area target="" alt="numero1" title="numero1" href="" coords="408,368,444,395" shape="rect">
-          <area target="" alt="numero2" title="numero2" href="" coords="472,383,42" shape="circle">
-          <area target="" alt="numero3" title="numero3" href="" coords="559,381,18" shape="circle">
-        </div>
-
-     
     </div>
   );
 }
